29558c231fdc7ae1c51966a895d3185d6d31de3c
ALEXANDER\Peter <pwyoung623@gmail.com>
2018-11-25 20:02:01 -0500

    Double checked that IFacingMover and GridMove scripts were present in this push.

---
+++ Dungeon Delver/Assets/__Scripts/GridMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMove : MonoBehaviour
+{
+    private IFacingMover mover;
+
+    void Awake()
+    {
+        mover = GetComponent<IFacingMover>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!mover.moving) return; //If not moving, nothing more to do
+        int facing = mover.GetFacing();
+
+        //If moving in a direction, aling to the grid
+        //First, get the grid locaiton
+        Vector2 rPos = mover.roomPos;
+        Vector2 rPosGrid = mover.GetRoomPosOnGrid();
+        //This relies on IFacingMover (which uses InRoom) to choose grid spacing
+
+        //Then move towards the grid line
+        float delta = 0;
+        if (facing == 0 || facing == 2)
+        {
+            //Horizontal movement, align to y grid
+            delta = rPosGrid.y - rPos.y;
+        } else
+        {
+            //Vertical movement, align to x grid
+            delta = rPosGrid.x - rPos.x;
+        }
+        if (delta == 0) return; // ALready aligned to the grid
+
+        float move = mover.GetSpeed() * Time.fixedDeltaTime;
+        move = Mathf.Min(move, Mathf.Abs(delta));
+        if (delta < 0) move = -move;
+
+        if(facing == 0 || facing == 2)
+        {
+            //Horizontal movement, align to y axis
+            rPos.y += move;
+        } else
+        {
+            //Vertical movement, align to x axis
+            rPos.x += move;
+        }
+
+        mover.roomPos = rPos;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+}
---
+++ Dungeon Delver/Assets/__Scripts/IFacingMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFacingMover {//a publci declaration of the IFacingMover interface
+    //publicly accessible methods and properties on any class that implements this interface. 
+    int GetFacing();
+    bool moving { get;  }
+    float GetSpeed();
+    float gridMult { get; }
+    Vector2 roomPos { get; set; }
+    Vector2 roomNum { get; set; }
+    //A default value for its mult parameter.
+    Vector2 GetRoomPosOnGrid(float mult = -1);
+}
