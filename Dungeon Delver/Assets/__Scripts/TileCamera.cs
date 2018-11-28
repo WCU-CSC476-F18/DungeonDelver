044037a4b6843c7d9099ac2f1f56390f1b7113c8
Joseph Miller <segintendo@yahoo.com>
2018-11-27 23:58:30 -0500

    Textbook prototype complete. "Implementing a New Dungeon-The Hat" part is complete. Dray can now go through an entire playable level. Now to add more levels...

--- Dungeon Delver/Assets/__Scripts/TileCamera.cs
+++ Dungeon Delver/Assets/__Scripts/TileCamera.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class TileSwap{
+    public int tileNum;
+    public GameObject swapPrefab;
+    public GameObject guaranteedItemDrop;
+    public int overrideTileNum = -1;
+}
+
 public class TileCamera : MonoBehaviour {
     static private int W, H;
     static private int[,] MAP;
@@ -15,10 +23,18 @@ public class TileCamera : MonoBehaviour {
     public Texture2D mapTiles;
     public TextAsset mapCollisions;
     public Tile tilePrefab;
+    public int defaultTileNum;
+    public List<TileSwap> tileSwaps;
+
+    private Dictionary<int, TileSwap> tileSwapDict;
+    private Transform enemyAnchor, itemAnchor;
 
     private void Awake()
     {
         COLLISIONS = Utils.RemoveLineEndings(mapCollisions.text);
+        PrepareTileSwapDict();
+        enemyAnchor = (new GameObject("EnemyAnchor")).transform;
+        itemAnchor = (new GameObject("Item Anchor")).transform;
         LoadMap();
     }
 
@@ -49,6 +65,8 @@ public class TileCamera : MonoBehaviour {
                     MAP[i, j] = 0;
                 else
                     MAP[i, j] = int.Parse(tileNums[i], hexNum);
+
+                CheckTileSwaps(i, j);
             }
         }
 
@@ -76,6 +94,51 @@ public class TileCamera : MonoBehaviour {
                 }
             }
         }
+    }
+
+    void PrepareTileSwapDict()
+    {
+        tileSwapDict = new Dictionary<int, TileSwap>();
+        foreach (TileSwap ts in tileSwaps)
+            tileSwapDict.Add(ts.tileNum, ts);
+    }
+
+    void CheckTileSwaps(int i, int j)
+    {
+        int tNum = GET_MAP(i, j);
+        if (!tileSwapDict.ContainsKey(tNum))
+            return;
+
+        //We do need to swap a tile
+        TileSwap ts = tileSwapDict[tNum];
+
+        if(ts.swapPrefab != null)
+        {
+            GameObject go = Instantiate(ts.swapPrefab);
+            Enemy e = go.GetComponent<Enemy>();
+
+            if(e != null)
+            {
+                go.transform.SetParent(enemyAnchor);
+            }
+            else
+            {
+                go.transform.SetParent(itemAnchor);
+            }
+            go.transform.position = new Vector3(i, j, 0);
+
+            if (ts.guaranteedItemDrop = ts.guaranteedItemDrop)
+            {
+                if (e != null)
+                    e.guaranteedItemDrop = ts.guaranteedItemDrop;
+            }
+        }
+
+        //Replace with another tile
+        if (ts.overrideTileNum == -1)
+            SET_MAP(i, j, defaultTileNum);
+        else
+            SET_MAP(i, j, ts.overrideTileNum);
     }
 
     static public int GET_MAP( int x, int y)
